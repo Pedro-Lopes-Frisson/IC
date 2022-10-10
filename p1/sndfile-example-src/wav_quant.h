0d7870d053b1a3954a734f6e77a0a0d64d13bec1
Pedro <pdfl@ua.pt>
2022-10-10 21:32:31 +0100

    Fix Wav Quant

--- p1/sndfile-example-src/wav_quant.h
+++ p1/sndfile-example-src/wav_quant.h
@@ -5,20 +5,22 @@
 #include <vector>
 #include <map>
 #include <sndfile.hh>
+#include <cmath>
 
 class WAVQuant {
-  private:
+private:
     int nBits;
-  public:
-    WAVQuant(const int n){
-      nBits = n;
+public:
+    WAVQuant(const int n) {
+        nBits = n;
     }
 
-  void apply_quantitization(std::vector<short> &samples){
-    for (size_t i = 0; i < samples.size(); i++){
-      samples[i] = samples[i] / (2 ^ nBits);
+    void apply_quantitization(std::vector<short> &samples) {
+        for (size_t i = 0; i < samples.size(); i++) {
+            samples[i] = (samples[i] >> (16 - nBits)) << (16 - nBits);
+        }
     }
-  }
 
 };
+
 #endif
