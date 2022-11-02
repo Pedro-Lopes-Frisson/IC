433b0f7afc69976576338d2c83397402b42c3fa0
Pedro <pdfl@ua.pt>
2022-11-02 16:55:26 +0000

    Golomb Encoder

---
+++ p2/GolombCoding.cpp
@@ -0,0 +1,5 @@
+//
+// Created by whoknows on 10/31/22.
+//
+
+#include "GolombCoding.h"
---
+++ p2/GolombCoding.h
@@ -0,0 +1,8 @@
+//
+// Created by whoknows on 10/31/22.
+//
+
+#ifndef P2_GOLOMBCODING_H
+#define P2_GOLOMBCODING_H
+
+#endif //P2_GOLOMBCODING_H
