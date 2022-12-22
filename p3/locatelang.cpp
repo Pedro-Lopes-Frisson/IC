ecdc9e8bdcb8736485f2177a3f4f09f6ce57d732
andre <andre.fernandes16@ua.pt>
2022-12-22 13:00:08 +0000

    Meter o executável

---
+++ p3/locatelang.cpp
@@ -0,0 +1,11 @@
+#include "fcm.h"
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+};
